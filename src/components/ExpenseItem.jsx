977ea97a14b0e1ff312854fb9c2dcdc12882f37f
Adam Davidson <agdavidson91@gmail.com>
2022-10-11 19:35:58 -0400

    setting up the skeleton

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
+const ExpenseItem = (props) => {
+  let expenseDate = new Date();
+
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
+  let todays = mm + "/" + dd + "/" + yyyy;
+
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 300.0;
+
+  return (
+    <div className="expense-item">
+      <div>{todays}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+      <div>ExpenseItem {props.names}</div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
